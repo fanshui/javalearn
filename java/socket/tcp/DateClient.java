8e5d6f9edf76cf5e51a3e63a4438caf8d17137ed
fanghui <fh006007@163.com>
2018-04-29 15:09:24 +0800

    nothing

--- java/socket/tcp/DateClient.java
+++ java/socket/tcp/DateClient.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
- * Created by fang on 2017/11/21.
+ * 
  */
 public class DateClient {
     public static void main(String[] args) {
